4c4eb1bba8f93c617af7a3997380a7e72d76aff0
isunyoo <isunyoo@gmail.com>
2021-05-05 14:16:46 +0800

    Updated for bug fixes

--- contracts/user.sol
+++ contracts/user.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
-// pragma solidity ^0.4.21;
 // import library file
 import "stringUtils.sol";
 // import "github.com/ethereum/dapp-bin/blob/master/library/stringUtils.sol";
@@ -16,7 +15,7 @@ contract userRecords {
   // user object
   user user_obj;
   //Internal function to conver genderType enum from string
-  function getGenderFromString(string gender) internal returns (genderType) {
+  function getGenderFromString(string memory gender) internal returns (genderType) {
     if(StringUtils.equal(gender, "male")) {
       return genderType.male;
     } else {
@@ -24,7 +23,7 @@ contract userRecords {
     }
   }
   //Internal function to convert genderType enum to string
-  function getGenderToString(genderType gender) internal returns (string) {
+  function getGenderToString(genderType gender) internal returns (string memory) {
     if(gender == genderType.male) {
       return "male";
     } else {
@@ -33,14 +32,14 @@ contract userRecords {
   }
   // set user public function
   // This is similar to persisting object in db.
-  function setUser(string name, string gender) public {
+  function setUser(string memory name, string memory gender) public {
     genderType gender_type = getGenderFromString(gender);
     user_obj = user({name:name, gender: gender_type});
   }
   
   // get user public function
   // This is similar to getting object from db.
-  function getUser() public returns (string, string) {
+  function getUser() public returns (string memory, string memory) {
     return (user_obj.name, getGenderToString(user_obj.gender));
   }
 }
